f89b497457af9e5b8d543aba581f1cb50e6b8678
Skulkrone <40754599+Skulkrone@users.noreply.github.com>
2022-10-31 09:51:45 +0100

    Update Slider.js
    ajout d'une logique de slider auto

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Slider.css";
 import dataSlider from "./dataSlider";
 import BtnSlider from "./BtnSlider";
@@ -8,6 +8,62 @@ export default function Slider() {
     index: 1,
     inProgress: false,
   });
+
+  // --------------- Anim auto Slider par Forth ------------
+
+  // Soit via une methode, soit directement dans le useEffect
+  // const autoSlide = () => {
+  //   const si = setInterval(nextSlide, 3000);
+  //   return () => clearInterval(si);
+  // };
+
+  // const nextSlide = () => {
+  //   // Je préfère cette vérif ici, comme ca on est trkl après
+  //   if (slideAnim.inProgress) return;
+
+  //   // J'ai retiré le if qui faisait doublon
+  //   // Je vérif l'index directement sur l'état actuel et non pas sur slideAnim,
+  //   // ca permet d'avoir la réelle valeur au moment t
+  //   setSlideAnim((anim) => ({
+  //     index: anim.index === dataSlider.length ? 1 : anim.index + 1,
+  //     inProgress: true,
+  //   }));
+
+  //   // ici on veut juste modifier inProgress
+  //   setTimeout(() => {
+  //     setSlideAnim((anim) => ({
+  //       ...anim,
+  //       inProgress: false,
+  //     }));
+  //   }, 400);
+  // };
+
+  // // memes changements que pour nextSlide
+  // const prevSlide = () => {
+  //   if (slideAnim.inProgress) return;
+
+  //   setSlideAnim((anim) => ({
+  //     index: anim.index === 1 ? dataSlider.length : anim.index - 1,
+  //     inProgress: true,
+  //   }));
+
+  //   setTimeout(() => {
+  //     setSlideAnim((anim) => ({
+  //       ...anim,
+  //       inProgress: false,
+  //     }));
+  //   }, 400);
+  // };
+
+  // const moveDot = (index) => {
+  //   setSlideAnim({ index, inProgress: false });
+  // };
+
+  // useEffect(() => {
+  //   // const si = setInterval(nextSlide, 3000);
+  //   // return () => clearInterval(si);
+  //   return autoSlide();
+  // }, []);
 
   const nextSlide = () => {
     if (slideAnim.index !== dataSlider.length && !slideAnim.inProgress) {
